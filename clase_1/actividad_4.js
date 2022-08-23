2ff4fe191fe41d4442120e47bf8dc71710dfcff1
majo-eguia <majitoeguia@gmail.com>
2022-08-23 20:10:00 -0300

    ✨ class 1 : General concepts: syntax and variables

---
+++ clase_1/actividad_4.js
@@ -0,0 +1,11 @@
+/* ACT 4 Solicitar al usuario cinco (5) nombres pertenecientes a integrantes de una familia.
+     Luego, concatenar dichas entradas y efectuar una salida por alerta.
+*/
+
+let integranteUno = prompt("Ingrese el nombre del primer integrante de la familia");
+let integranteDos = prompt("Ingrese el nombre del segundo integrante de la familia");
+let integranteTres = prompt("Ingrese el nombre del tercero integrante de la familia");
+let integranteCuatro = prompt("Ingrese el nombre del cuarto integrante de la familia");
+let integranteQuinto = prompt("Ingrese el nombre del quinto integrante de la familia");
+
+let salida = alert("La familia esta compuesta por: " + integranteUno  + ", " + integranteDos + ", " + integranteTres + ", " + integranteCuatro + " y " + integranteQuinto + ".");
