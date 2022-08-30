238a8b96f9a6a40d69c351a6851bc92e079ba988
majo-eguia <majitoeguia@gmail.com>
2022-08-30 18:03:07 -0300

    ✨ exercise 1 objects

--- class_5/exercise_1.js
+++ class_5/exercise_1.js
@@ -11,3 +11,39 @@ Por cada dato a registrar en un objeto, corresponde una propiedad.
  Recordemos que la invocación del objeto es instanciarlo usando new y el constructor.
 
 */
+
+class Tienda {
+  constructor(nombre, direccion, propietario, rubro) {
+    this.nombre = nombre;
+    this.direccion = direccion;
+    this.propietario = propietario;
+    this.rubro = rubro;
+  }
+}
+
+const tienda1 = new Tienda(
+  "Panchitos",
+  "Av rivadavia 123",
+  "Omar Perez",
+  "comestibles"
+);
+
+console.log(tienda1);
+
+const tienda2 = new Tienda(
+  "coto",
+  "Av lima 234",
+  "mister coto",
+  "supermercado"
+);
+
+console.log(tienda2);
+
+const tienda3 = new Tienda(
+  "adidas",
+  "Av roca 956",
+  "messi",
+  "zapatillas deportivas"
+);
+
+console.log(tienda3);
