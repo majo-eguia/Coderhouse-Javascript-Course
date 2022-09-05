70e39d1298039958346577b473824477475585dc
majo-eguia <majitoeguia@gmail.com>
2022-09-05 19:47:17 -0300

    🚧 Working on objects and arrays of the interactive simulator

--- interactive-simulator/interactive-simulator.js
+++ interactive-simulator/interactive-simulator.js
@@ -1,8 +1,59 @@
 /*
-Desarrollar un juego que le permita al participante adivinar la banda que
-compuso una canción.
-El participante tendrá hasta tres oportunidades.
+Desarrollar un juego de rondas que le permita al participante adivinar la banda que
+compuso una canción, esto dandole al participante un parrafo de la cancion
+que debe adivinar. Asi sucesivamente con varias canciones y bandas. Por cada ronda
+el participante tendrá hasta 3 vidas y cada vez que lo intente y sea erroneo
+perdera una vida. Cuando llegue a cero vidas  o cuando haya adivinado todas
+las rondas se acaba el juego.
+Cada vez que acierte sumara una cierta suma de dinero, la cual al finalizado el juego podra
+canjear por distintos premios de diferente valor, o elegir quedarse con el dinero.
+
 */
+
+class Cancion {
+  constructor(nombre, autor, año) {
+    this.nombre = nombre;
+    this.autor = autor;
+    this.genero = genero;
+    this.año = año;
+  }
+}
+
+const cancion1 = new Cancion("The show must go on", "Queen", "Rock", 1991);
+const cancion2 = new Cancion("I want it all", "Queen", "Rock", 1989);
+const cancion3 = new Cancion(
+  "Una nueva noche fria",
+  "Callejeros",
+  "Rock Nacional",
+  2003
+);
+const cancion4 = new Cancion("Creo", "Callejeros", "Rock Nacional", 2006);
+const cancion5 = new Cancion(
+  "Enano",
+  "Las pastillas del abuelo",
+  "Rock Nacional",
+  2006
+);
+
+const cancion6 = new Cancion(
+  "¿Qué carajo es el amor?",
+  "Las pastillas del abuelo",
+  "Rock Nacional",
+  2008
+);
+
+const cancion7 = new Cancion("Let it Be", "The Beatles", "Rock", 1970);
+const cancion8 = new Cancion("Help", "The Beatles", "Rock", 1965);
+
+const bandas = [
+  "Queen",
+  "Callejeros",
+  "Las pastillas del abuelo",
+  "The Beatles",
+];
+const adivinando = bandas.find(
+  (e) => e === prompt("¿Qué banda la compuso?").toLowerCase()
+);
 
 let cantidadDeIntentosRestantes = 3;
 let ganoElJuego = false;
@@ -41,7 +92,7 @@ while (leQuedanIntentos() && !ganoElJuego) {
 }
 
 if (ganoElJuego) {
-  alert("Felicitaciones, adivinaste. Ganaste un millón de dólares!");
+  alert("Felicitaciones, adivinaste la primer banda! Ganaste cien mil pesos!");
 } else {
   alert("Ya no tienes más intentos. Mejor suerte la próxima!");
 }
