b0a28f6c2bb66bd12d3fa119d94820628befe473
majo-eguia <majitoeguia@gmail.com>
2022-09-09 15:09:09 -0300

    ✨ improvements in the wording of the exercise

--- class_6/exercise_1.js
+++ class_6/exercise_1.js
@@ -4,8 +4,7 @@ integrantes de un equipo. Luego recorrer el array, e informar
 por alerta el nombre de cada jugador, 
 así como la posición  que ocupa en la colección.
 
-Cuando recorremos un array, 
-empleamos un bucle (preferentemente un for o for...of) 
+Cuando recorremos un array,empleamos un bucle (preferentemente un for o for...of) 
 para acceder a los elementos de la colección uno a uno.
 
 
