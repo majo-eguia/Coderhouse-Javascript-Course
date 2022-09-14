07d0b70485e85336542376768f13a8845e606522
majo-eguia <majitoeguia@gmail.com>
2022-09-13 21:55:51 -0300

    🚧 Improvements in the interactive simulator with new objects

--- interactive-simulator/interactive-simulator.js
+++ interactive-simulator/interactive-simulator.js
@@ -22,10 +22,145 @@ class FragmentoDeCancion {
     this.nombreDeLaBanda = nombreDeLaBanda;
   }
 
-  fueCompuestaPorLaBandaLlamada(nombreDeLaBanda){
-    return this.nombreDeLaBanda.toLowerCase() === nombreDeLaBanda.toLowerCase()
+  fueCompuestaPorLaBandaLlamada(nombreDeLaBanda) {
+    return this.nombreDeLaBanda.toLowerCase() === nombreDeLaBanda.toLowerCase();
   }
 }
+
+class Juego {
+  constructor(fragmentoDeCanciones, notificador) {
+    this.fragmentoDeCanciones = fragmentoDeCanciones;
+    this.notificador = notificador;
+  }
+
+  adivinoLaBandaEnLaRonda(fragmentoDeCancion, indiceDeRonda) {
+    const ronda = new Ronda(this, fragmentoDeCancion, indiceDeRonda + 1);
+    ronda.jugar();
+    return ronda.fueGanada;
+  }
+
+  presentar(fragmentoDeCancion) {
+    this.notificador.presentar(fragmentoDeCancion);
+  }
+
+  pasoLaRonda(ronda) {
+    this.notificador.pasoLaRonda(ronda);
+  }
+
+  noAdivinoYLeQuedan(cantidadDeIntentosRestantes) {
+    this.notificador.noAdivinoYLeQuedan(cantidadDeIntentosRestantes);
+  }
+
+  jugar() {
+    const fueGanado = this.fragmentoDeCanciones.every(
+      (fragmentoDeCancion, indiceDeRonda) =>
+        this.adivinoLaBandaEnLaRonda(fragmentoDeCancion, indiceDeRonda)
+    );
+    if (fueGanado) {
+      this.notificador.seGanoElJuego();
+    } else {
+      this.notificador.sePerdioElJuego();
+    }
+  }
+}
+
+class Ronda {
+  constructor(juego, fragmentoDeCancion, numeroDeRonda) {
+    this.juego = juego;
+    this.fragmentoDeCancion = fragmentoDeCancion;
+    this.numeroDeRonda = numeroDeRonda;
+    this.cantidadDeIntentosRestantes = 3;
+    this.fueGanada = false;
+  }
+
+  leQuedanIntentos() {
+    return this.cantidadDeIntentosRestantes > 0;
+  }
+
+  fueLaCancionCompuestaPorLaBandaLlamada(nombreDeLaBandaAdivinada) {
+    return this.fragmentoDeCancion.fueCompuestaPorLaBandaLlamada(
+      nombreDeLaBandaAdivinada
+    );
+  }
+
+  perdioUnIntento() {
+    this.cantidadDeIntentosRestantes--;
+  }
+
+  jugar() {
+    while (this.leQuedanIntentos() && !this.fueGanada) {
+      this.juego.presentar(this.fragmentoDeCancion);
+      const nombreDeLaBandaAdivinada = prompt("¿Qué banda la compuso?");
+      if (
+        this.fueLaCancionCompuestaPorLaBandaLlamada(nombreDeLaBandaAdivinada)
+      ) {
+        this.fueGanada = true;
+        this.juego.pasoLaRonda(this);
+      } else {
+        this.perdioUnIntento();
+        if (this.leQuedanIntentos()) {
+          this.juego.noAdivinoYLeQuedan(this.cantidadDeIntentosRestantes);
+        }
+      }
+    }
+  }
+}
+
+class NotificadorPorDOM {
+  constructor() {
+    this.main = document.createElement("main");
+    this.parrafoParaNotificarSiAdivinoLaBanda = document.createElement("p");
+    this.encabezadoParaPresentarLaCancion = document.createElement("h3");
+    this.parrafoDeFragmentoDeCancion = document.createElement("p");
+
+    this.main.appendChild(this.parrafoParaNotificarSiAdivinoLaBanda);
+    this.main.appendChild(this.encabezadoParaPresentarLaCancion);
+    this.main.appendChild(this.parrafoDeFragmentoDeCancion);
+    document.body.appendChild(this.main);
+  }
+
+  presentar(fragmentoDeCancion) {
+    this.encabezadoParaPresentarLaCancion.innerText =
+      "Esta es tu canción a adivinar...";
+    this.parrafoDeFragmentoDeCancion.innerText = fragmentoDeCancion.letra;
+  }
+
+  pasoLaRonda(ronda) {
+    this.parrafoParaNotificarSiAdivinoLaBanda.style.color = "green";
+    this.parrafoParaNotificarSiAdivinoLaBanda.innerText = `Correcto, la canción es ${ronda.fragmentoDeCancion.nombreDeLaCancion}, de la banda ${ronda.fragmentoDeCancion.nombreDeLaBanda}. Pasaste la ronda ${ronda.numeroDeRonda}.`;
+  }
+
+  noAdivinoYLeQuedan(cantidadDeIntentosRestantes) {
+    this.parrafoParaNotificarSiAdivinoLaBanda.style.color = "red";
+    this.parrafoParaNotificarSiAdivinoLaBanda.innerText = `Fallaste, pero no te rindas. Intentos restantes: ${cantidadDeIntentosRestantes}`;
+  }
+
+  seGanoElJuego() {
+    this.seTerminoElJuego(
+      "Felicitaciones, ganaste el juego! Ganaste cien mil pesos!",
+      "green"
+    );
+  }
+
+  sePerdioElJuego() {
+    this.seTerminoElJuego(
+      "Que lástima, perdiste el juego. Mejor suerte la próxima!",
+      "red"
+    );
+  }
+
+  seTerminoElJuego(resultado, colorDeTexto) {
+    let resultadoDelJuego = document.createElement("h3");
+    resultadoDelJuego.innerText = resultado;
+    resultadoDelJuego.style.color = colorDeTexto;
+    this.encabezadoParaPresentarLaCancion.remove();
+    this.parrafoDeFragmentoDeCancion.remove();
+    this.parrafoParaNotificarSiAdivinoLaBanda.remove();
+    this.main.appendChild(resultadoDelJuego);
+  }
+}
+
+// instanciación de fragmentos de canciones
 
 const fragmentoDeCancion1 = new FragmentoDeCancion(
   "Inside my heart is breaking\nMy makeup may be flaking\nBut my smile, still, stays on",
@@ -86,61 +221,8 @@ const fragmentoDeCanciones = [
   fragmentoDeCancion8,
 ];
 
-let leQuedanIntentos = (cantidadDeIntentosRestantes) =>
-  cantidadDeIntentosRestantes > 0;
-
-let presentarLaCancion = (fragmentoDeCancion) => {
-  alert("Esta es tu canción a adivinar...");
-  alert(fragmentoDeCancion.letra);
-};
-
-let adivinoLaBanda = (nombreDeLaBandaAdivinada, fragmentoDeCancion) => fragmentoDeCancion.fueCompuestaPorLaBandaLlamada(nombreDeLaBandaAdivinada);
-
-let notificarQueAdivino = (fragmentoDeCancion, numeroDeRonda) => {
-  alert(
-    "Correcto, la canción es " +
-      fragmentoDeCancion.nombreDeLaCancion +
-      ", de la banda " +
-      fragmentoDeCancion.nombreDeLaBanda +
-      ". Pasaste la ronda " +
-      (numeroDeRonda + 1) +
-      "."
-  );
-};
-
 // Inicia el juego
 
-alert(
-  "Esto es 'Conozco esa voz', el juego donde si adivinas la banda que compuso la canción, ganarás cien mil pesos. ¿Empezamos?"
-);
-
-let adivinoLaCancionEnLaRonda = (fragmentoDeCancion, numeroDeRonda) => {
-  let cantidadDeIntentosRestantes = 3;
-  let ganoLaRonda = false;
-  while (leQuedanIntentos(cantidadDeIntentosRestantes) && !ganoLaRonda) {
-    presentarLaCancion(fragmentoDeCancion);
-    const nombreDeLaBandaAdivinada = prompt("¿Qué banda la compuso?");
-    if (adivinoLaBanda(nombreDeLaBandaAdivinada, fragmentoDeCancion)) {
-      ganoLaRonda = true;
-      notificarQueAdivino(fragmentoDeCancion, numeroDeRonda);
-    } else {
-      cantidadDeIntentosRestantes--;
-      if (leQuedanIntentos(cantidadDeIntentosRestantes)) {
-        alert(
-          "Fallaste, pero no te rindas. Intentos restantes: " +
-            cantidadDeIntentosRestantes
-        );
-      }
-    }
-  }
-
-  return ganoLaRonda;
-};
-
-const ganoElJuego = fragmentoDeCanciones.every(adivinoLaCancionEnLaRonda);
-
-if (ganoElJuego) {
-  alert("Felicitaciones, ganaste el juego! Ganaste cien mil pesos!");
-} else {
-  alert("Que lástima, perdiste el juego. Mejor suerte la próxima!");
-}
+const notificadorPorDOM = new NotificadorPorDOM();
+const juego = new Juego(fragmentoDeCanciones, notificadorPorDOM);
+juego.jugar();
