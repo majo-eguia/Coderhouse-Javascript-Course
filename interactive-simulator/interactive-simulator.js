6417e4a19e1b5920aa9796c3d7a6d40840bad517
majo-eguia <majitoeguia@gmail.com>
2022-09-20 23:05:41 -0300

    🚚 muevo fragmento de canciones a un nuevo archivo

--- interactive-simulator/interactive-simulator.js
+++ interactive-simulator/interactive-simulator.js
@@ -15,70 +15,11 @@ Cada vez que acierte, el participante sumará una cierta suma de dinero, la cual
 canjear por distintos premios de diferente valor, o elegir quedarse con el dinero.
 */
 
-import FragmentoDeCancion from "./fragmento-de-cancion.js";
+import { fragmentoDeCanciones } from "./fragmento-de-canciones.js";
 import Juego from "./juego.js";
 import NotificadorPorDOM from "./notificador-por-dom.js";
 
 // instanciación de fragmentos de canciones
-
-const fragmentoDeCancion1 = new FragmentoDeCancion(
-  "Inside my heart is breaking\nMy makeup may be flaking\nBut my smile, still, stays on",
-  "The show must go on",
-  "Queen"
-);
-
-const fragmentoDeCancion2 = new FragmentoDeCancion(
-  "Listen all you people, come gather round\nI gotta get me a game plan, gotta shake you to the ground\nBut just give me, huh, what I know is mine\nPeople do you hear me, just gimme the sign",
-  "I want it all",
-  "Queen"
-);
-
-const fragmentoDeCancion3 = new FragmentoDeCancion(
-  "Solo como un pájaro que vuela en la noche\nLibre de vos, pero no de mí\nVacío como el sueño de una gorra lleno de nada\nSin saber donde ir",
-  "Una nueva noche fria",
-  "Callejeros"
-);
-
-const fragmentoDeCancion4 = new FragmentoDeCancion(
-  "Creo en tu sonrisa\nCreo en mí si te veo hoy\nY me pedis que no me rinda\nSigo por vos",
-  "Creo",
-  "Callejeros"
-);
-
-const fragmentoDeCancion5 = new FragmentoDeCancion(
-  "La vida lo educó a los bastonazos\ny a veces la muerte también,\ntuvo como profesor a un diablo bien pirata que\nle enseñó que la vida no siempre es ganar.",
-  "Enano",
-  "Las pastillas del abuelo"
-);
-
-const fragmentoDeCancion6 = new FragmentoDeCancion(
-  "Y si vos me preguntas hoy qué carajo es el amor\nYo te contesto: miralos a ellos dos\nY si vos me preguntas hoy qué carajo es el amor\nYo te contesto: miralos a ellos dos",
-  "¿Qué carajo es el amor?",
-  "Las pastillas del abuelo"
-);
-
-const fragmentoDeCancion7 = new FragmentoDeCancion(
-  "And in my hour of darkness\nShe is standing right in front of me\nSpeaking words of wisdom\nLet it be",
-  "Let it Be",
-  "The Beatles"
-);
-
-const fragmentoDeCancion8 = new FragmentoDeCancion(
-  "Help me if you can, I'm feeling down\nAnd I do appreciate you being 'round\nHelp me get my feet back on the ground\nWon't you please, please help me",
-  "Help",
-  "The Beatles"
-);
-
-const fragmentoDeCanciones = [
-  fragmentoDeCancion1,
-  fragmentoDeCancion2,
-  fragmentoDeCancion3,
-  fragmentoDeCancion4,
-  fragmentoDeCancion5,
-  fragmentoDeCancion6,
-  fragmentoDeCancion7,
-  fragmentoDeCancion8,
-];
 
 // Inicia el juego
 
